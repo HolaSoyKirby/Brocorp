130505406cfa1a571a6619c43dcd20b4f3cb2bc7
Tweek-Tweak <61298009+Tweek-Tweak@users.noreply.github.com>
2021-11-20 17:31:53 -0600

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBGErOb1fks6elvi7R-HxfKXH_8mgjs05A",
+    authDomain: "geo2-30946.firebaseapp.com",
+    projectId: "geo2-30946",
+    storageBucket: "geo2-30946.appspot.com",
+    messagingSenderId: "498631058607",
+    appId: "1:498631058607:web:bd54149ee3c681de97605a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
