30076be1d1964349e9b1c107f7d66dc7e5a18e53
Tweek-Tweak <61298009+Tweek-Tweak@users.noreply.github.com>
2021-11-20 17:27:07 -0600

    Add files via upload

---
+++ js/sidebar.js
@@ -0,0 +1,5 @@
+sidebarToggle.addEventListener('click', event => {
+    event.preventDefault();
+    document.body.classList.toggle('sb-sidenav-toggled');
+    localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+});
