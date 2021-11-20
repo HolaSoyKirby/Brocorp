3dcf3f1760267d39d8257927e2f7157e6eae0950
Tweek-Tweak <61298009+Tweek-Tweak@users.noreply.github.com>
2021-11-20 17:30:54 -0600

    Delete js directory

--- js/sidebar.js
+++
@@ -1,5 +0,0 @@
-sidebarToggle.addEventListener('click', event => {
-    event.preventDefault();
-    document.body.classList.toggle('sb-sidenav-toggled');
-    localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-});
